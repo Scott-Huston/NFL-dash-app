7c8a2e2855ca38626ae56b76fb2fa05bbed44502
Scott-Huston <51840874+Scott-Huston@users.noreply.github.com>
2019-08-28 10:26:42 -0700

    Updated title

--- app.py
+++ app.py
@@ -54,5 +54,5 @@ meta_tags=[
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, meta_tags=meta_tags)
 app.config.suppress_callback_exceptions = True
-app.title = 'YOUR APP NAME' # appears in browser title bar
+app.title = 'Play Predictor' # appears in browser title bar
 server = app.server
